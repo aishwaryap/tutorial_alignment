92a7664b9e827f7ca9320624cde52b89ac9eefba
Aishwarya <aishwarya.padmakumar.9.6@gmail.com>
2015-11-17 00:09:32 -0700

    Added stanford corenlp

--- hmm/recipe/TestRecipe.py
+++ hmm/recipe/TestRecipe.py
@@ -1,22 +1,42 @@
-import numpy
+import numpy, json, sys
+from pprint import pprint
+from nltk.tree import Tree
+
 from RecipeHMM import RecipeHMM
 
-def test():
-    observations = [[['a', 'cat'],['hello']],[['it', 'is', 'warm'],['a', 'dog'],['hello']]]
-    
-    atmp = numpy.random.random_sample((2, 2))
-    row_sums = atmp.sum(axis=1)
-    a = atmp / row_sums[:, numpy.newaxis]    
+sys.path.insert(0, '../../stanford-corenlp-python')
+from jsonrpc import ServerProxy, JsonRpc20, TransportTcpIp
 
-    pitmp = numpy.random.random_sample((2))
-    pi = pitmp / sum(pitmp)
+class StanfordNLP:
+    def __init__(self):
+        self.server = ServerProxy(JsonRpc20(),
+                                  TransportTcpIp(addr=("127.0.0.1", 8080)))
     
-    hmm = RecipeHMM(2, pi, a)
-    hmm.train(observations, 100)
-    print "Pi", hmm.pi
-    print "A", hmm.A
-    print "uni", hmm.uni
-    print "bi", hmm.bi
+    def parse(self, text):
+        return json.loads(self.server.parse(text))  
+
+def test():
+    nlp = StanfordNLP()
+    text = 'Hi, my name is Aishwarya. I am testing whether this works.'
+    parse = nlp.parse(text)
+    print parse[u'sentences'][0].keys()
+    print [str(word[0]) for word in parse[u'sentences'][0][u'words']]
+
+    #observations = [[['a', 'cat'],['hello']],[['it', 'is', 'warm'],['a', 'dog'],['hello']]]
+    
+    #atmp = numpy.random.random_sample((2, 2))
+    #row_sums = atmp.sum(axis=1)
+    #a = atmp / row_sums[:, numpy.newaxis]    
+
+    #pitmp = numpy.random.random_sample((2))
+    #pi = pitmp / sum(pitmp)
+    
+    #hmm = RecipeHMM(2, pi, a)
+    #hmm.train(observations, 100)
+    #print "Pi", hmm.pi
+    #print "A", hmm.A
+    #print "uni", hmm.uni
+    #print "bi", hmm.bi
 
 if __name__ == '__main__' :
     test()
